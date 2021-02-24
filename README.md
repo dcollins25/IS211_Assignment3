2f68b3546843d3c1219b24adc0da34804c456ae1
Daniel Collins <daniel.collins11@spsmail.cuny.edu>
2021-02-24 17:25:10 -0500

    IS211 - Assignment 3 - Dan Collins commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# IS211_Assignment3
-IS211_Assignment3
+# IS211_Assignment3 - Dan Collins
+
+	> git version 2.30.1.windows.1
+
+	> Python 3.6.5 :: Anaconda, Inc.
+
+# Forked Repository: https://github.com/cubanquant/IS211_Assignment3 
+
+# My copy for the assignment: https://github.com/dcollins25/IS211_Assignment3
+
+This script should run as planned ...
+	- use argparse module, requiring a URL as first position argument
+	- create a method to download the data
+	- use the method to retrieve the data from the URL passed on command-line
+	- decode into UTF-8
+	- put it into a dictionary
+	- use the re module to process the data, working out image counts and browser top usage
+
+	- Extra Credit: attempt to use datetime import time to process # of hits per hour (failed)
--- assignment3.py
+++ assignment3.py
@@ -1,15 +1,100 @@
 import argparse
-# other imports go here
+import urllib.request
+import csv
+import sys
+import re
+import operator
+from datetime import datetime
+from datetime import time
 
+# SetUp Argument Parser - script requires a first position argument of a URL
+parser = argparse.ArgumentParser()
+parser.add_argument("URL")
+link = parser.parse_args()
+print("\n\tDownloading data from: " + (link.URL))
+strurl = (link.URL)
 
-def main(url):
-    print(f"Running main with URL = {url}...")
+# Method to download and decode the data into UTF-8
+def downloadData(url):
+    req = urllib.request.urlopen(url)
+    decoded = str(req.read().decode('utf-8'))
+    decodedList = decoded.split("\n")
+    return decodedList
 
+# Runs the above method to get the data from the URL passed in by the user.
+# Input for this assignment: http://s3.amazonaws.com/cuny-is211-spring2015/weblog.csv
+# DATA: path to file, datetime accessed, browser, status of request, request size in bytes
 
-if __name__ == "__main__":
-    """Main entry point"""
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--url", help="URL to the datafile", type=str, required=True)
-    args = parser.parse_args()
-    main(args.url)
-    
+webData = downloadData(strurl)
+
+# Read the data into a dictionary
+csv_reader = csv.DictReader(webData)
+
+row_count = 0
+image_count = 0
+browser_count = 0
+firefox_count = 0
+chrome_count = 0
+ie_count = 0
+safari_count = 0
+
+# Regular Expression Searches
+regexpImage = re.compile(r'.PNG|.JPG|.GIF|.png|.jpg|.gif')
+regexpBrowser = re.compile(r'Firefox|Chrome|Internet Explorer|Safari')
+regexpHours = re.compile(r'[0-9]{2}:[0-9]{2}:[0-9]{2}')
+
+for row in csv_reader:
+    row_count += 1
+
+    rowList = list(row.items())
+
+    for x in rowList:
+        xList = list(x)
+        if regexpImage.search(xList[1]):
+            #print("FOUND IMAGE: " + xList[1])
+            image_count += 1
+        elif regexpBrowser.search(xList[1]):
+            #print("FOUND BROWSER: " +xList[1])
+            browser_count += 1
+            if "Firefox" in xList[1]:
+                #print("FOUND FIREFOX: ", xList[1])
+                firefox_count += 1
+            elif "Chrome" in xList[1]:
+                #print("FOUND CHROME: ", xList[1])
+                chrome_count += 1
+            elif "Internet Explorer" in xList[1]:
+                #print("FOUND IE: ", xList[1])
+                ie_count += 1
+            elif "Safari" in xList[1]:
+                #print("FOUND SAFARI: ", xList[1])
+                safari_count += 1
+        elif regexpHours.search(xList[1]):
+            #print("FOUND HOURS: " + xList[1])
+            splitXlist = xList[1].split(' ')
+            timeList = splitXlist[1].split(':')
+            hr = int(timeList[0])
+            min = int(timeList[1])
+            sec = int(timeList[2])
+
+# Failed attempt at using datetime here ...
+            rowTime = time(hr, min, sec)
+            #print("hour =", rowTime.hour)
+            #print("minute =", rowTime.minute)
+            #print("second =", rowTime.second)
+
+image_percentage = '{0:.2f}'.format((image_count / row_count * 100))
+imageStr = (str(image_percentage) + '%')
+print("\n\tImages account for", imageStr, "of all requests today")
+print("\t\t ... there were", image_count, "images found out of", row_count, "rows of data")
+
+#print("BROWSERS: ", browser_count)
+#print("FIREFOX: ", firefox_count)
+#print("CHROME: ", chrome_count)
+#print("IE: ", ie_count)
+#print("SAFARI: ", safari_count)
+
+browserStats = {"Firefox":firefox_count, "Chrome":chrome_count, "Internet Explorer":ie_count, "Safari":safari_count}
+print("\n\t", max(browserStats.items(), key=operator.itemgetter(1))[0], "had the highest amount of usage among browsers today")
+mostKeyStr = str(max(browserStats.items(), key=operator.itemgetter(1))[0])
+mostValueStr = mostKeyStr.strip()
+print("\t\t...it was used", browserStats[mostValueStr], "times")
